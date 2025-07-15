24eb82b1fd57445499229e55394c5b88d5dfe31e
Blananas2 <45607696+Blananas2@users.noreply.github.com>
2025-07-15 14:09:35 -0400

    some fixes, ty quinn

--- Assets/quoteCrazyTalkEndQuoteScript.cs
+++ Assets/quoteCrazyTalkEndQuoteScript.cs
@@ -62,55 +62,21 @@ public class quoteCrazyTalkEndQuoteScript : MonoBehaviour {
             SmallObjs[s].SetActive(false);
         }
 
-        retry:
-        int[] positions = { -1, -1, -1, -1 };
-        for (int p = 0; p < 4; p++)
+    retry:
+        int[] positions = Enumerable.Range(0, 100).Except(new[] { 0, 9, 90, 99 }).ToArray().Shuffle().Take(4).ToArray();
+        if (!positionsValid(positions))
         {
-            int v;
-            do
-            {
-                v = Rnd.Range(1, 99); //avoids 0 (top-left) and 99 (bottom-right)
-            } while (v == 9 || v == 90 || positions.Contains(v));
-            positions[p] = v;
-        }
-        if (!positionsValid(positions)) {
             goto retry;
         }
     }
 
     bool positionsValid(int[] a) //this is deceptively tricky
     {
-        /*
-        int[] xs = { -1, -1, -1, -1 };
-        int[] ys = { -1, -1, -1, -1 };
-        for (int e = 0; e < 4; e++)
-        {
-            xs[e] = a[e] % 10;
-            ys[e] = a[e] / 10;
-        }
-        int[] xb = { 10, -10 };
-        int[] yb = { 10, -10 };
-        for (int e = 0; e < 4; e++)
-        {
-            if (xs[e] < xb[0]) { xb[0] = xs[e]; }
-            if (xs[e] > xb[1]) { xb[1] = xs[e]; }
-            if (ys[e] < yb[0]) { yb[0] = ys[e]; }
-            if (ys[e] > yb[1]) { yb[1] = ys[e]; }
-        }
-        int[] edge = { -1, -1, -1, -1 };
-        for (int d = 0; d < 10; d++)
-        {
-            if (!edge.Contains(-1)) { break; }
-            for (int e = 0; e < 4; e++) {
-                if (ys[e] == d) {  }
-            }
-        }
-        */
         return true;
     }
 
     void MainPress() {
-        Debug.Log("ommmmmmmmm");
+        Debug.Log("om");
     }
 
     void SmallPress(int s) {
